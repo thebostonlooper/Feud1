b72c3334c5a8a57554a1d05120b818dfbc641b0e
thebostonlooper <103009243+thebostonlooper@users.noreply.github.com>
2023-04-24 21:09:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const fs = require('fs');
 const session = require('express-session');
 
 dotenv.config();
@@ -16,79 +15,12 @@ app.use(session({
 }));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to Family Feud Game API!');
+  res.send('Welcome to Family Feud Game!'); // Modified the message
 });
 
 const PORT = process.env.PORT || 5001;
 
-const questions = JSON.parse(fs.readFileSync('questions.json', 'utf8'));
-
-const getQuestion = () => {
-  const today = new Date();
-  const dayOfYear = Math.floor(
-    (today - new Date(today.getFullYear(), 0, 0)) / (1000 * 60 * 60 * 24)
-  );
-  const index = dayOfYear % questions.length;
-  const question = questions[index];
-  return question;
-};
-
-const calculateScore = (userAnswer, correctAnswers) => {
-  const isCorrect = correctAnswers.some(
-    (answer) => answer.toLowerCase() === userAnswer.toLowerCase()
-  );
-  return isCorrect ? 1 : 0;
-};
-
-//app.get('/api/start', async (req, res) => {
-app.get('/.netlify/functions/start', async (req, res) => {
-  const question = getQuestion();
-  req.session.score = 0;
-  req.session.wrongGuesses = 0;
-  req.session.correctAnswers = question.answers;
-  res.json({ question: question.question, answers: question.answers.length });
-});
-
-//app.post('/api/submit-answer', async (req, res) => {
-app.post('/.netlify/functions/submitAnswer', async (req, res) => {
-    const userAnswer = req.body.answer;
-    const isCorrect = req.session.correctAnswers.some(
-      (answer, index) => {
-        const found = answer.toLowerCase() === userAnswer.toLowerCase();
-        if (found) {
-          req.session.answerIndex = index;
-        }
-        return found;
-      }
-    );
-  
-    if (isCorrect) {
-      req.session.score += 1;
-    } else {
-      req.session.wrongGuesses += 1;
-    }
-  
-    res.json({
-      isCorrect,
-      score: req.session.score,
-      wrongGuesses: req.session.wrongGuesses,
-      answerIndex: req.session.answerIndex,
-      correctAnswer: isCorrect ? req.session.correctAnswers[req.session.answerIndex] : null,
-      totalAnswers: req.session.correctAnswers.length // Add this line
-    });
-  });  
-
-//app.get('/api/next-prompt', async (req, res) => {
-app.get('/.netlify/functions/nextPrompt', async (req, res) => {
-  const question = getQuestion();
-  res.json({ prompt: question.question });
-});
-
-//app.get("/api/get-all-answers", async (req, res) => {
-app.get("/.netlify/functions/getAllAnswers", async (req, res) => {
-    res.json({ allAnswers: req.session.correctAnswers });
-  });  
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
